9e69ccea531323b93aed7976e58a54a8982d6c86
grigaut <70752330+grigaut@users.noreply.github.com>
2023-02-17 15:28:53 +0100

    Feature/issue-4/enable-specific-date-range-for-mapping (#31)

--- scripts/plot_mesh.py
+++ scripts/plot_mesh.py
@@ -2,16 +2,16 @@ import datetime as dt
 import sys
 
 from bgc_data_processing import CONFIG, data_providers
-from bgc_data_processing.base import Storer
+from bgc_data_processing.data_classes import Storer
 from bgc_data_processing.tracers import GeoMesher
 
 BIN_SIZE = tuple(CONFIG["MAPPING"]["BINS_SIZE"])
 
 
 def get_args(sys_argv: list) -> tuple[list[str], list[str], int]:
-    year_min = int(sys_argv[1])
-    year_max = int(sys.argv[2])
-    var_name = sys.argv[3]
+    date_start = dt.datetime.strptime(sys_argv[1], "%Y%m%d").date()
+    date_end = dt.datetime.strptime(sys_argv[2], "%Y%m%d").date()
+    var_name = sys_argv[3]
     if len(sys_argv) > 4:
         list_src = sys_argv[4].split(",")
     else:
@@ -23,12 +23,12 @@ def get_args(sys_argv: list) -> tuple[list[str], list[str], int]:
             verbose = 0
     else:
         verbose = 1
-    return year_min, year_max, var_name, list_src, verbose
+    return date_start, date_end, var_name, list_src, verbose
 
 
 if __name__ == "__main__":
     # Script arguments
-    YEAR_MIN, YEAR_MAX, VAR_NAME, LIST_SRC, VERBOSE = get_args(sys.argv)
+    DATE_MIN, DATE_MAX, VAR_NAME, LIST_SRC, VERBOSE = get_args(sys.argv)
     data_dict = {}
     for data_src in LIST_SRC:
         if VERBOSE > 0:
@@ -36,8 +36,8 @@ if __name__ == "__main__":
         exclude = CONFIG["LOADING"][data_src]["EXCLUDE"]
         dset_loader = data_providers.LOADERS[data_src]
         dset_loader.set_date_boundaries(
-            date_min=dt.datetime(YEAR_MIN, 1, 1),
-            date_max=dt.datetime(YEAR_MAX, 12, 31),
+            date_min=DATE_MIN,
+            date_max=DATE_MAX,
         )
         dset_loader.set_latitude_boundaries(
             latitude_min=50,
