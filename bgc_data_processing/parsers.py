edfec4ccc9f52c8259df8dcab61c75705659e78a
Gaétan Rigaut <gaetanrigaut.s@gmail.com>
2023-02-21 16:00:56 +0100

    docs: documentation of ConfigParser

--- bgc_data_processing/parsers.py
+++ bgc_data_processing/parsers.py
@@ -93,13 +93,36 @@ class RanCycleParser:
         return drng
 
 
-class TOMLParser:
+class ConfigParser:
+    """Parsing class for config.toml
+
+    Parameters
+    ----------
+    filepath : str
+        Path to the config file.
+    """
+
     def __init__(self, filepath: str) -> None:
         self.filepath = filepath
         with open(filepath, "rb") as f:
             self.config = tomllib.load(f)
 
     def _check_type(self, var: Any, var_type: Type | tuple[Type, Type]) -> bool:
+        """Checks if the type of the variable correspond to the required type.
+
+        Parameters
+        ----------
+        var : Any
+            Variable to check type.
+        var_type : Type | tuple[Type, Type]
+            Type for the variable, if tuple, means that the first value is an ierable
+            and the second one the type of the values in the iterable.
+
+        Returns
+        -------
+        bool
+            True if the variable matches the type, False otherwise.
+        """
         if isinstance(var_type, tuple):
             is_correct_iterator = isinstance(var, var_type[0])
             is_all_correct = all(isinstance(x, var_type[1]) for x in var)
@@ -108,6 +131,21 @@ class TOMLParser:
             return isinstance(var, var_type)
 
     def _make_error_msg(self, keys: list[str], types: list[Type]) -> str:
+        """Creates error message for TypeErrors
+
+        Parameters
+        ----------
+        keys : list[str]
+            List path to the variable: ["VAR1", "VAR2", "VAR3"]
+            is the path to the variable VAR1.VAR2.VAR3 in the toml.
+        types : list[Type]
+            Correct types for the variable.
+
+        Returns
+        -------
+        str
+            Error message to pass to TypeError.
+        """
         wrong_type_msg = f"Variable {'.'.join(keys)} from {self.filepath} does not have the correct type."
         crop = lambda x: str(str(x).split("'")[1])
         iterables = [t for t in types if isinstance(t, tuple)]
@@ -118,6 +156,24 @@ class TOMLParser:
         return f"{wrong_type_msg} {correct_type_msg}"
 
     def get(self, keys: list[str]) -> Any:
+        """Return a variable from the toml using its path.
+
+        Parameters
+        ----------
+        keys : list[str]
+            List path to the variable: ["VAR1", "VAR2", "VAR3"]
+            is the path to the variable VAR1.VAR2.VAR3 in the toml.
+
+        Returns
+        -------
+        Any
+            Variable.
+
+        Raises
+        ------
+        KeyError
+            If the path doesn't match the file's architecture
+        """
         if keys[0] not in self.config.keys():
             raise KeyError(
                 f"Variable {'.'.join(keys[:1])} does not exist in {self.filepath}"
@@ -136,8 +192,27 @@ class TOMLParser:
         self,
         keys: list[str],
         var_type: Type | tuple[Type, Type],
-        *args,
+        *args: Type | tuple[Type, Type],
     ) -> None:
+        """Raises a TypeError if the variable type is none of the specified types.
+
+        Parameters
+        ----------
+        keys : list[str]
+            List path to the variable: ["VAR1", "VAR2", "VAR3"]
+            is the path to the variable VAR1.VAR2.VAR3 in the toml.
+        var_type : Type | tuple[Type, Type]
+            Type for the variable, if tuple, means that the first value is an ierable
+            and the second one the type of the values in the iterable.
+        args : Type | tuple[Type, Type]
+            Additional possible types for the variable, if tuple, means that the first value is an ierable
+            and the second one the type of the values in the iterable.
+
+        Raises
+        ------
+        TypeError
+            If the variable doesn't match any of the required types.
+        """
         var = self.get(keys)
         types = [var_type] + list(args)
         # Check type:
@@ -146,14 +221,28 @@ class TOMLParser:
             raise TypeError(self._make_error_msg(keys=keys, types=types))
 
     @property
-    def aggregation(self):
+    def aggregation(self) -> dict:
+        """Data-aggregation related part of the toml.
+
+        Returns
+        -------
+        dict
+            self.config["AGGREGATION"]
+        """
         self.raise_if_wrong_type(["AGGREGATION", "YEARS"], (list, int))
         self.raise_if_wrong_type(["AGGREGATION", "PROVIDERS"], str, (list, str))
         self.raise_if_wrong_type(["AGGREGATION", "LIST_DIR"], str)
         return self.get(["AGGREGATION"])
 
     @property
-    def mapping(self):
+    def mapping(self) -> dict:
+        """Data-mapping related part of the toml.
+
+        Returns
+        -------
+        dict
+            self.config["MAPPING"] with converted date times
+        """
         mapping = self.get(["MAPPING"])
         self.raise_if_wrong_type(["MAPPING", "DATE_MIN"], str)
         self.raise_if_wrong_type(["MAPPING", "DATE_MAX"], str)
@@ -166,7 +255,14 @@ class TOMLParser:
         return mapping
 
     @property
-    def providers(self):
+    def providers(self) -> dict:
+        """Providers related part of the toml.
+
+        Returns
+        -------
+        dict
+            self.config["PROVIDERS"]
+        """
         for provider in self.config["PROVIDERS"]:
             self.raise_if_wrong_type(["PROVIDERS", provider, "PATH"], str)
             self.raise_if_wrong_type(["PROVIDERS", provider, "CATEGORY"], str)
@@ -175,6 +271,13 @@ class TOMLParser:
 
     @property
     def utils(self):
+        """'Utils'' part of the toml.
+
+        Returns
+        -------
+        dict
+            self.config["UTILS"]
+        """
         self.raise_if_wrong_type(["UTILS", "VERBOSE"], int)
         self.raise_if_wrong_type(["UTILS", "SAVING_DIR"], str)
         saving_dir = self.get(["UTILS", "SAVING_DIR"])
