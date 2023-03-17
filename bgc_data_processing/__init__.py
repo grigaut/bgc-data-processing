0a65f6f2ff6c46e73dddbb1a65ac0a938596c218
Gaétan Rigaut <gaetanrigaut.s@gmail.com>
2023-03-17 15:52:47 +0100

    refactor: load config files in scripts

--- bgc_data_processing/__init__.py
+++ bgc_data_processing/__init__.py
@@ -1,24 +1,9 @@
 from bgc_data_processing import parsers
 
 __all__ = [
-    "SAVE_CONFIG",
-    "PLOT_CONFIG",
     "PROVIDERS_CONFIG",
     "DEFAULT_VARS",
 ]
-
-SAVE_CONFIG = parsers.ConfigParser(
-    "config/save_data.toml",
-    True,
-    ["DATE_MIN", "DATE_MAX"],
-    ["SAVING_DIR"],
-)
-PLOT_CONFIG = parsers.ConfigParser(
-    "config/plot_mesh.toml",
-    True,
-    ["DATE_MIN", "DATE_MAX"],
-    ["SAVING_DIR"],
-)
 PROVIDERS_CONFIG = parsers.ConfigParser("config/providers.toml", True)
 
 DEFAULT_VARS = parsers.DefaultTemplatesParser(
