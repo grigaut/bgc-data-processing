f835b6bfda35bf18a2d3071488e9afcf67267979
Gaétan Rigaut <gaetanrigaut.s@gmail.com>
2023-05-03 13:15:45 +0200

    feat: import WaterMasse in bgc_data_processing __init__ file

--- src/bgc_data_processing/__init__.py
+++ src/bgc_data_processing/__init__.py
@@ -5,10 +5,15 @@ from bgc_data_processing import parsers
 __all__ = [
     "PROVIDERS_CONFIG",
     "DEFAULT_VARS",
+    "DEFAULT_WATER_MASSES",
 ]
 PROVIDERS_CONFIG = parsers.ConfigParser("config/providers.toml", True)
 
 DEFAULT_VARS = parsers.DefaultTemplatesParser(
     filepath="config/variables.toml",
+    check_types=True,
+)
+DEFAULT_WATER_MASSES = parsers.WaterMassesParser(
+    filepath="config/water_masses.toml",
     check_types=True,
 )
