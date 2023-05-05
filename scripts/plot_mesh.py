4da486860587f864f3e5e61d72cd72286a4d4da7
grigaut <70752330+grigaut@users.noreply.github.com>
2023-05-05 14:21:47 +0200

    refactor: remove loading and plotting scripts (#190)

--- scripts/plot_mesh.py
+++ scripts/plot_mesh.py
@@ -1,20 +1,23 @@
-"""Data 2D mesh plotting script."""
+"""Create a 2d Mesh Plot from files in a directory."""
 
 import datetime as dt
+import os
 
-from bgc_data_processing import PROVIDERS_CONFIG, data_providers, parsers
 from bgc_data_processing.data_classes import Constraints, Storer
+from bgc_data_processing.parsers import ConfigParser
 from bgc_data_processing.tracers import MeshPlotter
 
 if __name__ == "__main__":
-    # Script arguments
-    CONFIG = parsers.ConfigParser(
-        "config/plot_mesh.toml",
-        check_types=True,
+    CONFIG = ConfigParser(
+        filepath="config/plot_mesh.toml",
         dates_vars_keys=["DATE_MIN", "DATE_MAX"],
         dirs_vars_keys=["SAVING_DIR"],
         existing_directory="raise",
     )
+    LOADING_DIR: str = CONFIG["LOADING_DIR"]
+    VARIABLE: str = CONFIG["VARIABLE"]
+    SHOW: bool = CONFIG["SHOW"]
+    SAVE: bool = CONFIG["SAVE"]
     DATE_MIN: dt.datetime = CONFIG["DATE_MIN"]
     DATE_MAX: dt.datetime = CONFIG["DATE_MAX"]
     LATITUDE_MIN: int | float = CONFIG["LATITUDE_MIN"]
@@ -28,86 +31,83 @@ if __name__ == "__main__":
     DEPTH_MIN: int | float = CONFIG["DEPTH_MIN"]
     DEPTH_MAX: int | float = CONFIG["DEPTH_MAX"]
     BIN_SIZE: list | int | float = CONFIG["BIN_SIZE"]
-    VARIABLE: str = CONFIG["VARIABLE"]
-    PROVIDERS: list[str] = CONFIG["PROVIDERS"]
-    PRIORITY: list[str] = CONFIG["PRIORITY"]
     CONSIDER_DEPTH: bool = CONFIG["CONSIDER_DEPTH"]
     EXPOCODES_TO_LOAD: list[str] = CONFIG["EXPOCODES_TO_LOAD"]
-    SHOW: bool = CONFIG["SHOW"]
-    SAVE: bool = CONFIG["SAVE"]
+    PRIORITY: list[str] = CONFIG["PRIORITY"]
     VERBOSE: int = CONFIG["VERBOSE"]
 
-    data_dict = {}
-    for data_src in PROVIDERS:
-        if VERBOSE > 0:
-            print(f"Loading data : {data_src}")
-        exclude = PROVIDERS_CONFIG[data_src]["EXCLUDE"]
-        dset_loader = data_providers.LOADERS[data_src]
-        variables = dset_loader.variables
-        constraints = Constraints()
-        constraints.add_superset_constraint(
-            field_label=variables.get(variables.expocode_var_name).label,
-            values_superset=EXPOCODES_TO_LOAD,
+    filepaths = [
+        f"{LOADING_DIR}/{file}"
+        for file in os.listdir(LOADING_DIR)
+        if file[-4:] in [".csv", ".txt"]
+    ]
+
+    storer = Storer.from_files(
+        filepath=filepaths,
+        providers_column_label="PROVIDER",
+        expocode_column_label="EXPOCODE",
+        date_column_label="DATE",
+        year_column_label="YEAR",
+        month_column_label="MONTH",
+        day_column_label="DAY",
+        hour_column_label="HOUR",
+        latitude_column_label="LATITUDE",
+        longitude_column_label="LONGITUDE",
+        depth_column_label="DEPH",
+        category="in_situ",
+        unit_row_index=1,
+        delim_whitespace=True,
+        verbose=1,
+    )
+    storer.remove_duplicates(PRIORITY)
+    variables = storer.variables
+    constraints = Constraints()
+    constraints.add_superset_constraint(
+        field_label=variables.get(variables.expocode_var_name).label,
+        values_superset=EXPOCODES_TO_LOAD,
+    )
+    constraints.add_boundary_constraint(
+        field_label=variables.get(variables.date_var_name).label,
+        minimal_value=DATE_MIN,
+        maximal_value=DATE_MAX,
+    )
+    constraints.add_boundary_constraint(
+        field_label=variables.get(variables.latitude_var_name).label,
+        minimal_value=LATITUDE_MIN,
+        maximal_value=LATITUDE_MAX,
+    )
+    constraints.add_boundary_constraint(
+        field_label=variables.get(variables.longitude_var_name).label,
+        minimal_value=LONGITUDE_MIN,
+        maximal_value=LONGITUDE_MAX,
+    )
+    constraints.add_boundary_constraint(
+        field_label=variables.get(variables.depth_var_name).label,
+        minimal_value=DEPTH_MIN,
+        maximal_value=DEPTH_MAX,
+    )
+    plot = MeshPlotter(storer, constraints=constraints)
+    plot.set_density_type(consider_depth=CONSIDER_DEPTH)
+    plot.set_bins_size(bins_size=BIN_SIZE)
+    plot.set_map_boundaries(
+        latitude_min=LATITUDE_MAP_MIN,
+        latitude_max=LATITUDE_MAP_MAX,
+        longitude_min=LONGITUDE_MAP_MIN,
+        longitude_max=LONGITUDE_MAP_MAX,
+    )
+    date_min = DATE_MIN.strftime("%Y%m%d")
+    date_max = DATE_MAX.strftime("%Y%m%d")
+    if SHOW:
+        suptitle = f"{VARIABLE} - from {LOADING_DIR}\n" f"{date_min}-{date_max}"
+        plot.show(
+            variable_name=VARIABLE,
+            suptitle=suptitle,
         )
-        constraints.add_boundary_constraint(
-            field_label=variables.get(variables.date_var_name).label,
-            minimal_value=DATE_MIN,
-            maximal_value=DATE_MAX,
+    if SAVE:
+        save_name = f"density_map_{VARIABLE}_{date_min}_{date_max}.png"
+        suptitle = f"{VARIABLE} - from {LOADING_DIR}\n" f"{date_min}-{date_max}"
+        plot.save(
+            save_path=f"{CONFIG['SAVING_DIR']}/{save_name}",
+            variable_name=VARIABLE,
+            suptitle=suptitle,
         )
-        constraints.add_boundary_constraint(
-            field_label=variables.get(variables.latitude_var_name).label,
-            minimal_value=LATITUDE_MIN,
-            maximal_value=LATITUDE_MAX,
-        )
-        constraints.add_boundary_constraint(
-            field_label=variables.get(variables.longitude_var_name).label,
-            minimal_value=LONGITUDE_MIN,
-            maximal_value=LONGITUDE_MAX,
-        )
-        constraints.add_boundary_constraint(
-            field_label=variables.get(variables.depth_var_name).label,
-            minimal_value=DEPTH_MIN,
-            maximal_value=DEPTH_MAX,
-        )
-        dset_loader.set_verbose(VERBOSE)
-        storer = dset_loader(exclude=exclude, constraints=constraints)
-        category = dset_loader.category
-        if category not in data_dict.keys():
-            data_dict[category] = []
-        data_dict[category].append(storer)
-    for category, data in data_dict.items():
-        if VERBOSE > 0:
-            print(f"Plotting {category} data")
-        storer: Storer = sum(data)
-        storer.remove_duplicates(priority_list=PRIORITY)
-        date_min = DATE_MIN.strftime("%Y%m%d")
-        date_max = DATE_MAX.strftime("%Y%m%d")
-        plot = MeshPlotter(storer, constraints=constraints)
-        plot.set_density_type(consider_depth=CONSIDER_DEPTH)
-        plot.set_bins_size(bins_size=BIN_SIZE)
-        plot.set_map_boundaries(
-            latitude_min=LATITUDE_MAP_MIN,
-            latitude_max=LATITUDE_MAP_MAX,
-            longitude_min=LONGITUDE_MAP_MIN,
-            longitude_max=LONGITUDE_MAP_MAX,
-        )
-        if SHOW:
-            suptitle = (
-                f"{VARIABLE} - {', '.join(PROVIDERS)} ({category})\n"
-                f"{date_min}-{date_max}"
-            )
-            plot.show(
-                variable_name=VARIABLE,
-                suptitle=suptitle,
-            )
-        if SAVE:
-            save_name = f"density_map_{VARIABLE}_{date_min}_{date_max}.png"
-            suptitle = (
-                f"{VARIABLE} - {', '.join(PROVIDERS)} ({category})\n"
-                f"{date_min}-{date_max}"
-            )
-            plot.save(
-                save_path=f"{CONFIG['SAVING_DIR']}/{save_name}",
-                variable_name=VARIABLE,
-                suptitle=suptitle,
-            )
--- scripts/plot_profile.py
+++ scripts/plot_profile.py
@@ -1,93 +1,99 @@
-"""Evolution profile plotting Script."""
+"""Create an Evolution Profile from files in a directory."""
 
 import datetime as dt
+import os
 
-from bgc_data_processing import PROVIDERS_CONFIG, data_providers, parsers
 from bgc_data_processing.data_classes import Constraints, Storer
+from bgc_data_processing.parsers import ConfigParser
 from bgc_data_processing.tracers import EvolutionProfile
 
 if __name__ == "__main__":
-    # Script arguments
-    CONFIG = parsers.ConfigParser(
-        "config/plot_profile.toml",
-        check_types=True,
+    CONFIG = ConfigParser(
+        filepath="config/plot_profile.toml",
         dates_vars_keys=["DATE_MIN", "DATE_MAX"],
         dirs_vars_keys=["SAVING_DIR"],
         existing_directory="raise",
     )
+    LOADING_DIR: str = CONFIG["LOADING_DIR"]
+    VARIABLE: str = CONFIG["VARIABLE"]
+    SHOW: bool = CONFIG["SHOW"]
+    SAVE: bool = CONFIG["SAVE"]
     DATE_MIN: dt.datetime = CONFIG["DATE_MIN"]
     DATE_MAX: dt.datetime = CONFIG["DATE_MAX"]
-    DEPTH_MIN: int | float = CONFIG["DEPTH_MIN"]
-    DEPTH_MAX: int | float = CONFIG["DEPTH_MAX"]
     LATITUDE_MIN: int | float = CONFIG["LATITUDE_MIN"]
     LATITUDE_MAX: int | float = CONFIG["LATITUDE_MAX"]
     LONGITUDE_MIN: int | float = CONFIG["LONGITUDE_MIN"]
     LONGITUDE_MAX: int | float = CONFIG["LONGITUDE_MAX"]
+    DEPTH_MIN: int | float = CONFIG["DEPTH_MIN"]
+    DEPTH_MAX: int | float = CONFIG["DEPTH_MAX"]
     INTERVAL: str = CONFIG["INTERVAL"]
     CUSTOM_INTERVAL: int = CONFIG["CUSTOM_INTERVAL"]
     DEPTH_INTERVAL: int = CONFIG["DEPTH_INTERVAL"]
-    VARIABLE: str = CONFIG["VARIABLE"]
-    PROVIDERS: list[str] = CONFIG["PROVIDERS"]
-    EXPOCODES_TO_LOAD: list[str] = CONFIG["EXPOCODES_TO_LOAD"]
     PRIORITY: list[str] = CONFIG["PRIORITY"]
-    SHOW: bool = CONFIG["SHOW"]
-    SAVE: bool = CONFIG["SAVE"]
+    EXPOCODES_TO_LOAD: list[str] = CONFIG["EXPOCODES_TO_LOAD"]
     VERBOSE: int = CONFIG["VERBOSE"]
 
-    data_dict = {}
-    for data_src in PROVIDERS:
-        if VERBOSE > 0:
-            print(f"Loading data : {data_src}")
-        exclude = PROVIDERS_CONFIG[data_src]["EXCLUDE"]
-        dset_loader = data_providers.LOADERS[data_src]
-        variables = dset_loader.variables
-        constraints = Constraints()
-        constraints.add_superset_constraint(
-            field_label=variables.get(variables.expocode_var_name).label,
-            values_superset=EXPOCODES_TO_LOAD,
+    filepaths = [
+        f"{LOADING_DIR}/{file}"
+        for file in os.listdir(LOADING_DIR)
+        if file[-4:] in [".csv", ".txt"]
+    ]
+
+    storer = Storer.from_files(
+        filepath=filepaths,
+        providers_column_label="PROVIDER",
+        expocode_column_label="EXPOCODE",
+        date_column_label="DATE",
+        year_column_label="YEAR",
+        month_column_label="MONTH",
+        day_column_label="DAY",
+        hour_column_label="HOUR",
+        latitude_column_label="LATITUDE",
+        longitude_column_label="LONGITUDE",
+        depth_column_label="DEPH",
+        category="in_situ",
+        unit_row_index=1,
+        delim_whitespace=True,
+        verbose=1,
+    )
+    storer.remove_duplicates(PRIORITY)
+    storer.remove_duplicates(priority_list=PRIORITY)
+    variables = storer.variables
+    constraints = Constraints()
+    constraints.add_superset_constraint(
+        field_label=variables.get(variables.expocode_var_name).label,
+        values_superset=EXPOCODES_TO_LOAD,
+    )
+    constraints.add_boundary_constraint(
+        field_label=variables.get(variables.date_var_name).label,
+        minimal_value=DATE_MIN,
+        maximal_value=DATE_MAX,
+    )
+    constraints.add_boundary_constraint(
+        field_label=variables.get(variables.latitude_var_name).label,
+        minimal_value=LATITUDE_MIN,
+        maximal_value=LATITUDE_MAX,
+    )
+    constraints.add_boundary_constraint(
+        field_label=variables.get(variables.longitude_var_name).label,
+        minimal_value=LONGITUDE_MIN,
+        maximal_value=LONGITUDE_MAX,
+    )
+    constraints.add_boundary_constraint(
+        field_label=variables.get(variables.depth_var_name).label,
+        minimal_value=DEPTH_MIN,
+        maximal_value=DEPTH_MAX,
+    )
+    profile = EvolutionProfile(storer, constraints=constraints)
+    profile.set_date_intervals(INTERVAL, CUSTOM_INTERVAL)
+    profile.set_depth_interval(DEPTH_INTERVAL)
+    if SHOW:
+        profile.show(VARIABLE)
+    if SAVE:
+        date_min_str = DATE_MIN.strftime("%Y%m%d")
+        date_max_str = DATE_MAX.strftime("%Y%m%d")
+        save_name = f"profile_{VARIABLE}_{date_min_str}_{date_max_str}.png"
+        profile.save(
+            save_path=f"{CONFIG['SAVING_DIR']}/{save_name}",
+            variable_name=VARIABLE,
         )
-        constraints.add_boundary_constraint(
-            field_label=variables.get(variables.date_var_name).label,
-            minimal_value=DATE_MIN,
-            maximal_value=DATE_MAX,
-        )
-        constraints.add_boundary_constraint(
-            field_label=variables.get(variables.latitude_var_name).label,
-            minimal_value=LATITUDE_MIN,
-            maximal_value=LATITUDE_MAX,
-        )
-        constraints.add_boundary_constraint(
-            field_label=variables.get(variables.longitude_var_name).label,
-            minimal_value=LONGITUDE_MIN,
-            maximal_value=LONGITUDE_MAX,
-        )
-        constraints.add_boundary_constraint(
-            field_label=variables.get(variables.depth_var_name).label,
-            minimal_value=DEPTH_MIN,
-            maximal_value=DEPTH_MAX,
-        )
-        dset_loader.set_verbose(VERBOSE)
-        storer = dset_loader(exclude=exclude, constraints=constraints)
-        category = dset_loader.category
-        if category not in data_dict.keys():
-            data_dict[category] = []
-        data_dict[category].append(storer)
-    for category, data in data_dict.items():
-        if VERBOSE > 0:
-            print(f"Plotting {category} data")
-        storer: Storer = sum(data)
-        storer.remove_duplicates(priority_list=PRIORITY)
-        variables = storer.variables
-        profile = EvolutionProfile(storer, constraints=constraints)
-        profile.set_date_intervals(INTERVAL, CUSTOM_INTERVAL)
-        profile.set_depth_interval(DEPTH_INTERVAL)
-        if SHOW:
-            profile.show(VARIABLE)
-        if SAVE:
-            date_min_str = DATE_MIN.strftime("%Y%m%d")
-            date_max_str = DATE_MAX.strftime("%Y%m%d")
-            save_name = f"profile_{VARIABLE}_{date_min_str}_{date_max_str}.png"
-            profile.save(
-                save_path=f"{CONFIG['SAVING_DIR']}/{save_name}",
-                variable_name=VARIABLE,
-            )
