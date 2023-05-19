c5b9282273c59a526377ba94494aabcd8038f17c
Gaétan Rigaut <gaetanrigaut.s@gmail.com>
2023-05-19 13:56:34 +0200

    feat: add selective lodaing pipeline

--- src/bgc_data_processing/selectors.py
+++ src/bgc_data_processing/selectors.py
@@ -1,184 +1,280 @@
 """Data selectors objects."""
 
-from abc import ABC, abstractmethod
 
 import numpy as np
 import pandas as pd
-from abfile import ABFileGrid
 from sklearn.neighbors import NearestNeighbors
 
-
-class BaseSelector(ABC):
-    """Base Class for selectors.
-
-    Parameters
-    ----------
-    reference : pd.DataFrame
-        Reference data.
-    reference_lat_field : str
-        Name of the latitude column in reference.
-    reference_lon_field : str
-        Name of the longitude column in reference.
-    """
-
-    def __init__(
-        self,
-        reference: pd.DataFrame,
-        reference_lat_field: str,
-        reference_lon_field: str,
-    ) -> None:
-
-        self.reference = reference
-        self.ref_lat = reference_lat_field
-        self.ref_lon = reference_lon_field
-
-    @abstractmethod
-    def select_closest(self, lat: pd.Series, lon: pd.Series) -> np.ndarray:
-        """Select closest points.
-
-        Parameters
-        ----------
-        lat : pd.Series
-            Latitude Series to find the closest of.
-        lon : pd.Series
-            Longitude Series to find the closest of.
-
-        Returns
-        -------
-        np.ndarray
-            Index of closest samples.
-        """
-
-    @abstractmethod
-    def select_from_abfile(
-        self,
-        grid_abfile_basename: str,
-        lat_field: str,
-        lon_field: str,
-    ) -> np.ndarray:
-        """Select closest points in an abfile.
-
-        Parameters
-        ----------
-        grid_abfile_basename: str
-            Basename of the gridfile.
-        lat_field: str
-            Name of the latitude field.
-        lon_field: str
-            Name of the longitude field.
-
-        Returns
-        -------
-        np.ndarray
-            2D mask to indicate data to keep.
-        """
+from bgc_data_processing.abfiles_tools import ABFileLoader, SelectiveABFileLoader
+from bgc_data_processing.data_classes import Constraints, Storer
 
 
-class NearestNeighborSelector(BaseSelector):
-    """Closest point selection using sklearn's nearest neighbors.
+class NearestNeighborStrategy:
+    """Implement a closest point search using NearestNeighbor algorithm.
 
     Parameters
     ----------
-    reference : pd.DataFrame
-        Reference data.
-    reference_lat_field : str
-        Name of the latitude column in reference.
-    reference_lon_field : str
-        Name of the longitude column in reference.
-    **kwargs:
-        Additional arhuments ot pass to NearestNeighbors
+    **model_kwargs:
+        Additional arguments to pass to sklearn.neighbors.NearestNeighbors.
+        The value of 'n_neighbors' while be overridden by 1.
     """
 
-    def __init__(
-        self,
-        reference: pd.DataFrame,
-        reference_lat_field: str,
-        reference_lon_field: str,
-        **kwargs,
-    ) -> None:
+    def __init__(self, **model_kwargs) -> None:
+        model_kwargs["n_neighbors"] = 1
+        self.model_kwargs = model_kwargs
 
-        super().__init__(reference, reference_lat_field, reference_lon_field)
-        self.model_kwargs = kwargs
-        self.model_kwargs["n_neighbors"] = 1
-
-    def select_from_abfile(
+    def get_closest_indexes(
         self,
-        grid_abfile_basename: str,
-        lat_field: str,
-        lon_field: str,
-    ) -> np.ndarray:
-        """Select closest points in an abfile.
+        simulations_lat_lon: pd.DataFrame,
+        observations_lat_lon: pd.DataFrame,
+    ) -> pd.Series:
+        """Find closest simulation point for each observation point.
 
         Parameters
         ----------
-        grid_abfile_basename: str
-            Basename of the gridfile.
-        lat_field: str
-            Name of the latitude field.
-        lon_field: str
-            Name of the longitude field.
+        simulations_lat_lon : pd.DataFrame
+            DataFrame with longitude and latitude for each simulations point.
+        observations_lat_lon : pd.DataFrame
+            DataFrame with longitude and latitude for each observation point.
 
         Returns
         -------
-        np.ndarray
-            2D mask to indicate data to keep.
-        """
-        grid_file = ABFileGrid(grid_abfile_basename, "r")
-        lat = self.read_raw_field(grid_file, lat_field)
-        lon = self.read_raw_field(grid_file, lon_field)
-        y_coords, x_coords = np.meshgrid(range(lat.shape[1]), range(lat.shape[0]))
-        x_coords_series = pd.Series(x_coords.flatten())
-        y_coords_series = pd.Series(y_coords.flatten())
-        lat_series = pd.Series(lat.flatten(), name=self.ref_lat)
-        lon_series = pd.Series(lon.flatten(), name=self.ref_lon)
-        index = self.select_closest(lat=lat_series, lon=lon_series)
-        selected_xs = x_coords_series.loc[index]
-        selected_ys = y_coords_series.loc[index]
-        to_keep = np.full(shape=lat.shape, fill_value=False)
-        to_keep[selected_xs, selected_ys] = True
-        return to_keep
-
-    def read_raw_field(self, file: ABFileGrid, field_name: str) -> np.ndarray:
-        """Read a grid file field.
-
-        Parameters
-        ----------
-        file : ABFileGrid
-            File to read the field of.
-        field_name : str
-            Name of the field to read.
-
-        Returns
-        -------
-        np.ndarray
-            Data from the file.
-        """
-        mask_2d: np.ma.masked_array = file.read_field(field_name)
-        return mask_2d.filled(np.nan)
-
-    def select_closest(
-        self,
-        lat: pd.Series,
-        lon: pd.Series,
-    ) -> np.ndarray:
-        """Select closest points.
-
-        Parameters
-        ----------
-        lat : pd.Series
-            Latitude Series to find the closest of.
-        lon : pd.Series
-            Longitude Series to find the closest of.
-
-        Returns
-        -------
-        np.ndarray
-            Index of closest samples.
+        pd.Series
+            Index of closest point for every observation point.
         """
         model = NearestNeighbors(**self.model_kwargs)
-        model.fit(X=pd.concat([lat, lon], axis=1))
+        model.fit(X=simulations_lat_lon)
         closest = model.kneighbors(
-            self.reference,
+            observations_lat_lon,
             return_distance=False,
         )
-        return closest.flatten()
+        return pd.Series(closest.flatten(), index=observations_lat_lon.index)
+
+
+class Mask:
+    """Mask to apply to ABFiles to filter data while loading.
+
+    Parameters
+    ----------
+    mask_2d : np.ndarray
+        2D array to mask layers when loading them.
+    index_2d : np.ndarray
+        2D array of indexes to use to reindex the filtered array.
+
+    Raises
+    ------
+    ValueError
+        If the mask and the index have a different shape.
+    """
+
+    def __init__(self, mask_2d: np.ndarray, index_2d: np.ndarray) -> None:
+        if mask_2d.shape != index_2d.shape:
+            raise ValueError("Both mask and index must have similar shapes.")
+        self.mask = mask_2d
+        self._index = pd.Index(index_2d[self.mask].flatten())
+
+    @property
+    def index(self) -> pd.Index:
+        """Index for masked data reindexing.
+
+        Returns
+        -------
+        pd.Index
+            Data Index.
+        """
+        return self._index
+
+    def __call__(self, data_2d: np.ndarray, **kwargs) -> pd.Series:
+        """Apply mask to 2D data.
+
+        Parameters
+        ----------
+        data_2d : np.ndarray
+            Data to apply the mask to.
+        **kwargs:
+            Additional parameters to pass to pd.Series.
+            The value of 'index' while be overridden by self._index.
+
+        Returns
+        -------
+        pd.Series
+            Masked data as a pd.Series with self._index as index.
+        """
+        kwargs["index"] = self._index
+        return pd.Series(data_2d[self.mask].flatten(), **kwargs)
+
+
+class Match:
+    """Match between observation indexes and simulations indexes.
+
+    Parameters
+    ----------
+    obs_closests_indexes : pd.Series
+        Closest simulated point index Series.
+        The index is supposed to correspond to observations' index.
+    """
+
+    index_simulated: str = "sim_index"
+    index_observed: str = "obs_index"
+    index_loaded: str = "load_index"
+
+    def __init__(self, obs_closests_indexes: pd.Series) -> None:
+
+        index_link = obs_closests_indexes.to_frame(name=self.index_simulated)
+        index_link.index.name = self.index_observed
+        index_link.reset_index(inplace=True)
+        self.index_link = index_link
+
+    def match(self, loaded_df: pd.DataFrame) -> pd.DataFrame:
+        """Transform the DataFrame index to link it to observations' index.
+
+        Parameters
+        ----------
+        loaded_df : pd.DataFrame
+            DataFrame to change the index of.
+
+        Returns
+        -------
+        pd.DataFrame
+            Copy of loaded_df with a modified index, which correspond to
+            observations' index values.
+        """
+        loaded_index = pd.Series(loaded_df.index, name=self.index_simulated).to_frame()
+        loaded_index.index.name = self.index_loaded
+        loaded_index.reset_index(inplace=True)
+        loaded_copy = loaded_df.copy()
+        loaded_copy.index = loaded_index.index
+        merge = pd.merge(
+            left=loaded_index,
+            right=self.index_link,
+            left_on=self.index_simulated,
+            right_on=self.index_simulated,
+            how="left",
+        )
+        reshaped = loaded_copy.loc[merge[self.index_loaded], :]
+        reshaped.index = merge[self.index_observed].values
+        return reshaped
+
+
+class Selector:
+    """Load closest datapoints from a reference dataframe.
+
+    Parameters
+    ----------
+    reference : pd.DataFrame
+        Reference Dataframe (observations).
+    strategy : NearestNeighborStrategy
+        Closer point finding strategy.
+    loader : ABFileLoader
+        Loader.
+    """
+
+    def __init__(
+        self,
+        reference: pd.DataFrame,
+        strategy: NearestNeighborStrategy,
+        loader: "ABFileLoader",
+    ) -> None:
+        self.reference = reference
+        self.loader = loader
+        self.strategy = strategy
+        self.grid = self.loader.grid_file
+
+    def get_coord(self, var_name: str) -> pd.Series:
+        """Get a coordinate field from loader.grid_file.
+
+        Parameters
+        ----------
+        var_name : str
+            Name of the variable to retrieve.
+
+        Returns
+        -------
+        pd.Series
+            Loaded variable as pd.Series.
+
+        Raises
+        ------
+        ValueError
+            If the variable dosn't exist in the grid file.
+        """
+        var = self.loader.variables.get(var_name)
+        found = False
+        for alias, _, _ in var.aliases:
+            if alias in self.grid.fieldnames:
+                mask_2d: np.ma.masked_array = self.grid.read_field(alias)
+                found = True
+                break
+        if not found:
+            raise ValueError
+        value = mask_2d.filled(np.nan)
+        return pd.Series(value.flatten(), name=var.label)
+
+    def get_x_y_indexes(self) -> tuple[pd.Series, pd.Series]:
+        """Get x and y indexes.
+
+        Returns
+        -------
+        tuple[pd.Series, pd.Series]
+            X indexes series, Y indexes series.
+        """
+        y_coords, x_coords = np.meshgrid(range(self.grid.idm), range(self.grid.jdm))
+        x_coords_series = pd.Series(x_coords.flatten())
+        y_coords_series = pd.Series(y_coords.flatten())
+        return x_coords_series, y_coords_series
+
+    def select(
+        self,
+    ) -> tuple["Mask", "Match"]:
+        """Select closest points in an abfile using self.strategy.
+
+        Returns
+        -------
+        tuple[Mask, Match]
+            Mask to use for loader, Match to link observations to simulations.
+        """
+        lat_series = self.get_coord(self.loader.variables.latitude_var_name)
+        lon_series = self.get_coord(self.loader.variables.longitude_var_name)
+        sims = pd.concat([lat_series, lon_series], axis=1)
+        x_coords_series, y_coords_series = self.get_x_y_indexes()
+        index = self.strategy.get_closest_indexes(
+            simulations_lat_lon=sims,
+            observations_lat_lon=self.reference[sims.columns],
+        )
+        selected_xs = x_coords_series.loc[index.values]
+        selected_ys = y_coords_series.loc[index.values]
+        to_keep = np.full(shape=(self.grid.jdm, self.grid.idm), fill_value=False)
+        to_keep[selected_xs, selected_ys] = True
+        indexes = np.array(range(self.grid.jdm * self.grid.idm))
+        indexes_2d = indexes.reshape((self.grid.jdm, self.grid.idm))
+        return Mask(to_keep, indexes_2d), Match(index)
+
+    def __call__(
+        self,
+        constraints: "Constraints" = Constraints(),
+        exclude: list[str] = [],
+    ) -> "Storer":
+        """Load all files for the loader.
+
+        Parameters
+        ----------
+        constraints : Constraints, optional
+            Constraints slicer., by default Constraints()
+        exclude : list, optional
+            Files not to load., by default []
+
+        Returns
+        -------
+        Storer
+            Storer for the loaded data.
+        """
+        mask, match = self.select()
+        loader = SelectiveABFileLoader.from_abloader(loader=self.loader, mask=mask)
+        sims = loader(constraints=constraints, exclude=exclude)
+        return Storer(
+            data=match.match(sims.data),
+            category=sims.category,
+            providers=sims.providers,
+            variables=sims.variables,
+            verbose=sims.verbose,
+        )
