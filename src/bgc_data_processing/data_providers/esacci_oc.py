76c61ae246a34a38d14fe6552ce0db75c8ca9222
Gaétan Rigaut <gaetanrigaut.s@gmail.com>
2023-06-15 08:33:45 +0200

    feat: ESACCI-OC loader definition

---
+++ src/bgc_data_processing/data_providers/esacci_oc.py
@@ -0,0 +1,36 @@
+"""Specific parameters to load ESACCI-OC-provided data."""
+
+from pathlib import Path
+
+from bgc_data_processing import (
+    DEFAULT_VARS,
+    PROVIDERS_CONFIG,
+    loaders,
+    variables,
+)
+
+loader = loaders.from_netcdf(
+    provider_name="ESACCI-OC",
+    dirin=Path(PROVIDERS_CONFIG["ESACCI-OC"]["PATH"]),
+    category=PROVIDERS_CONFIG["ESACCI-OC"]["CATEGORY"],
+    files_pattern="{years}/.*.nc",
+    variables=variables.VariablesStorer(
+        provider=DEFAULT_VARS["provider"].not_in_file(),
+        expocode=DEFAULT_VARS["expocode"].not_in_file(),
+        date=DEFAULT_VARS["date"].in_file_as("time"),
+        year=DEFAULT_VARS["year"].not_in_file(),
+        month=DEFAULT_VARS["month"].not_in_file(),
+        day=DEFAULT_VARS["day"].not_in_file(),
+        hour=DEFAULT_VARS["hour"].not_in_file(),
+        longitude=DEFAULT_VARS["longitude"].in_file_as("lon"),
+        latitude=DEFAULT_VARS["latitude"].in_file_as("lat"),
+        depth=DEFAULT_VARS["depth"].not_in_file().set_default(0),
+        temperature=DEFAULT_VARS["temperature"].not_in_file(),
+        salinity=DEFAULT_VARS["salinity"].not_in_file(),
+        oxygen=DEFAULT_VARS["oxygen"].not_in_file(),
+        phosphate=DEFAULT_VARS["phosphate"].not_in_file(),
+        nitrate=DEFAULT_VARS["nitrate"].not_in_file(),
+        silicate=DEFAULT_VARS["silicate"].not_in_file(),
+        chlorophyll=DEFAULT_VARS["chlorophyll"].in_file_as("chlor_a").remove_when_nan(),
+    ),
+)
